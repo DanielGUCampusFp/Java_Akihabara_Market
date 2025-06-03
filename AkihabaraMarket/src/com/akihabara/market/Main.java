1d1adaca4434232385314151637375ec89580c80
Daniel González Ureña <daniel.gonzalezurena24@campusfp.es>
2025-06-03 09:26:58 +0200

    Add files via upload

---
+++ AkihabaraMarket/src/com/akihabara/market/Main.java
@@ -0,0 +1,16 @@
+package com.akihabara.market;
+
+import com.akihabara.market.dao.DatabaseConnection;
+
+/**
+ * Clase temporal para probar la conexión a la base de datos.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Crear instancia para establecer conexión
+        DatabaseConnection dbConn = new DatabaseConnection();
+
+        // Cerrar la conexión
+        dbConn.cerrarConexion();
+    }
+}
